c0748209b42757d2648a2afe1b311221e9c26cdb
Nyene-Abasi <nyeneabasi3@gmail.com>
2023-06-30 10:33:43 +0100

    used error handler to handle error for non type integer

--- models/model.js
+++ models/model.js
@@ -71,11 +71,6 @@ function selectAllArticles  () {
 
       
      function deleteComment(comment_id) {
-
-      if (isNaN(comment_id)) {
-         return Promise.reject({ status: 400, msg: 'Bad request' });
-       }
-     
        return db
        .query('DELETE FROM comments WHERE comment_id = $1', [comment_id])
        .then(({ rowCount }) => {
